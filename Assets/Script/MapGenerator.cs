37550e46e406a0cd6e5a4d4aa9634b4b75a21ca7
alexisgodbillot <114652313+alexxterrieur@users.noreply.github.com>
2024-10-11 12:03:44 +0200

    Procedural map 2
    using tilemaps
    octave/ persistence/ lacunarity

--- Assets/Script/MapGenerator.cs
+++ Assets/Script/MapGenerator.cs
@@ -1,27 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class MapGenerator : MonoBehaviour
 {
-    Dictionary<int, GameObject> tileset;
-    Dictionary<int, GameObject> tileGroups;
+    Dictionary<int, Tile> tileset;
 
     [Header("Tiles")]
-    [SerializeField] private GameObject prefabSoil;
-    [SerializeField] private GameObject prefabGrass;
-    [SerializeField] private GameObject prefabWater;
-    [SerializeField] private GameObject prefabMountains;
+    [SerializeField] private Tile soilTile;
+    [SerializeField] private Tile grassTile;
+    [SerializeField] private Tile waterTile;
+    [SerializeField] private Tile mountainTile;
+
+    public Tilemap groundTilemap;
+    public Tilemap waterTilemap;
 
     [Header("Map Size")]
     [SerializeField] private int mapWidth;
     [SerializeField] private int mapHeight;
 
+    private float centerX;
+    private float centerY;
+
     List<List<int>> noiseGrid = new List<List<int>>();
-    List<List<GameObject>> tileGrid = new List<List<GameObject>>();
 
     [Header("Noise Parameters")]
-    [Range(4, 20)] public float magnification = 7.0f;
+    [Range(1, 20)] public float noiseScale;
+    [Range(1, 10)] public int numOctaves;
+    [Range(0.0f, 1.0f)] public float persistence;
+    [Range(1.0f, 3.0f)] public float lacunarity;
 
     [Header("Islandify")]
     public bool islandify = false;
@@ -32,52 +40,46 @@ public class MapGenerator : MonoBehaviour
 
     void Start()
     {
+        centerX = mapWidth / 2f;
+        centerY = mapHeight / 2f;
+
         CreateTileset();
-        CreateTileGroups();
         GenerateMap();
     }
 
     void CreateTileset()
     {
-        tileset = new Dictionary<int, GameObject>();
-        tileset.Add(0, prefabSoil);
-        tileset.Add(1, prefabGrass);
-        tileset.Add(2, prefabWater);
-        tileset.Add(3, prefabMountains);
-    }
-
-    void CreateTileGroups()
-    {
-        tileGroups = new Dictionary<int, GameObject>();
-        foreach (KeyValuePair<int, GameObject> prefabPair in tileset)
-        {
-            GameObject tileGroup = new GameObject(prefabPair.Value.name);
-            tileGroup.transform.parent = gameObject.transform;
-            tileGroup.transform.localPosition = new Vector3(0, 0, 0);
-            tileGroups.Add(prefabPair.Key, tileGroup);
-        }
+        tileset = new Dictionary<int, Tile>();
+        tileset.Add(0, soilTile);
+        tileset.Add(1, grassTile);
+        tileset.Add(2, waterTile);
+        tileset.Add(3, mountainTile);
     }
 
     void GenerateMap()
     {
+        int xOffset = mapWidth / 2;
+        int yOffset = mapHeight / 2;
+
         for (int x = 0; x < mapWidth; x++)
         {
             noiseGrid.Add(new List<int>());
-            tileGrid.Add(new List<GameObject>());
 
             for (int y = 0; y < mapHeight; y++)
             {
+                //tile ID according to the noise
                 int tileId = islandify ? GetIdForIsland(x, y) : GetIdUsingPerlin(x, y);
                 noiseGrid[x].Add(tileId);
-                CreateTile(tileId, x, y);
+
+                CreateTile(tileId, x - xOffset, y - yOffset);
             }
         }
     }
 
     int GetIdUsingPerlin(int x, int y)
     {
-        float rawPerlin = Mathf.PerlinNoise((x - xOffset) / magnification, (y - yOffset) / magnification);
-        float clampPerlin = Mathf.Clamp01(rawPerlin);
+        float perlinValue = GetPerlinNoiseWithOctaves(x, y);
+        float clampPerlin = Mathf.Clamp01(perlinValue);
         float scaledPerlin = clampPerlin * tileset.Count;
 
         if (scaledPerlin == tileset.Count)
@@ -85,6 +87,27 @@ public class MapGenerator : MonoBehaviour
             scaledPerlin = (tileset.Count - 1);
         }
         return Mathf.FloorToInt(scaledPerlin);
+    }
+
+    float GetPerlinNoiseWithOctaves(int x, int y)
+    {
+        float total = 0;
+        float frequency = 1;
+        float amplitude = 1;
+        float normalizedValue = 0;
+
+        for (int i = 0; i < numOctaves; i++)
+        {
+            float perlinValue = Mathf.PerlinNoise((x - xOffset) / (noiseScale * frequency), (y - yOffset) / (noiseScale * frequency)) * amplitude;
+            total += perlinValue;
+
+            normalizedValue += amplitude;
+
+            amplitude *= persistence;
+            frequency *= lacunarity;
+        }
+
+        return total / normalizedValue;
     }
 
     int GetIdForIsland(int x, int y)
@@ -106,15 +129,18 @@ public class MapGenerator : MonoBehaviour
         }
     }
 
-    void CreateTile(int tile_id, int x, int y)
+    void CreateTile(int tileId, int x, int y)
     {
-        GameObject tilePrefab = tileset[tile_id];
-        GameObject tileGroup = tileGroups[tile_id];
-        GameObject tile = Instantiate(tilePrefab, tileGroup.transform);
+        Tile tile = tileset[tileId];
 
-        tile.name = string.Format("tile_x{0}_y{1}", x, y);
-        tile.transform.localPosition = new Vector3(x, y, 0);
-
-        tileGrid[x].Add(tile);
+        //check if is water or land
+        if (tileId == 2)
+        {
+            waterTilemap.SetTile(new Vector3Int(x, y, 0), tile);
+        }
+        else
+        {
+            groundTilemap.SetTile(new Vector3Int(x, y, 0), tile);
+        }
     }
 }
